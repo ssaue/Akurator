8431e0a5e19a7e952a76f24ac63d6f13b802d062
ssaue <sigurd.saue@gmail.com>
2019-01-09 10:48:39 +0100

    Added linear map value

--- Source/sspLinearMap.cpp
+++ Source/sspLinearMap.cpp
@@ -9,3 +9,67 @@
 */
 
 #include "sspLinearMap.h"
+#include "sspLogging.h"
+
+sspLinearMap::sspLinearMap()
+	: inp_min_(0.0f), inp_max_(1.0f), outp_min_(0.0f), outp_max_(1.0f)
+{
+	computeLinearFactors();
+}
+
+void sspLinearMap::computeLinearFactors()
+{
+	// Avoid division by zero
+	lin_a_ = std::abs(inp_max_ - inp_min_) > std::numeric_limits<float>::epsilon() 
+		? (outp_max_ - outp_min_) / (inp_max_ - inp_min_) : 1.0f;
+	lin_b_ = outp_max_ - lin_a_ * inp_max_;
+}
+
+void sspLinearMap::setInputRange(float fMin, float fMax) 
+{ 
+	// Should not be inverted
+	if (fMin > fMax) {
+		inp_min_ = fMax;
+		inp_max_ = fMin;
+	}
+	else {
+		inp_min_ = fMin;
+		inp_max_ = fMax;
+	}
+	computeLinearFactors();
+}
+void sspLinearMap::setOutputRange(float fMin, float fMax)
+{ 
+	// May be inverted
+	outp_min_ = fMin; 
+	outp_max_ = fMax; 
+	computeLinearFactors();
+}
+
+float sspLinearMap::getValue() const
+{
+	return lin_a_ * val_->getValue() + lin_b_;
+}
+
+bool sspLinearMap::verify(int & nErrors, int & nWarnings) const
+{
+	bool bReturn = true;
+
+	if (!val_) {
+		SSP_LOG_WRAPPER_ERROR(nErrors, bReturn) << getName() << " has invalid value";
+	}
+	else if (val_.get() == this) {
+		SSP_LOG_WRAPPER_ERROR(nErrors, bReturn) << getName() << " has a self reference";
+	}
+	if (inp_min_ > inp_max_) {
+		SSP_LOG_WRAPPER_ERROR(nErrors, bReturn) << getName() << ": input range is inverted";
+	}
+	else if ((inp_max_ - inp_min_) < std::numeric_limits<float>::epsilon()) {
+		SSP_LOG_WRAPPER_ERROR(nErrors, bReturn) << getName() << ": input has zero range";
+	}
+	if (std::abs(inp_max_ - inp_min_) < std::numeric_limits<float>::epsilon()) {
+		SSP_LOG_WRAPPER_WARNING(nWarnings, bReturn) << getName() << ": output has zero range";
+	}
+
+	return bReturn;
+}
--- Source/sspLinearMap.h
+++ Source/sspLinearMap.h
@@ -9,3 +9,54 @@
 */
 
 #pragma once
+
+#include "sspPrimitives.h"
+#include <boost/serialization/shared_ptr.hpp>
+
+class sspLinearMap : public sspValue
+{
+	std::shared_ptr<sspValue> val_;
+
+	float inp_min_, inp_max_;
+	float outp_min_, outp_max_;
+
+	// Utility variables to save computation time
+	float lin_a_, lin_b_;
+	void computeLinearFactors();
+
+	friend class boost::serialization::access;
+	template <typename Archive>
+	void serialize(Archive & ar, const unsigned int /*version*/) {
+		ar & BOOST_SERIALIZATION_BASE_OBJECT_NVP(sspValue);
+		ar & BOOST_SERIALIZATION_NVP(val_);
+		ar & BOOST_SERIALIZATION_NVP(inp_min_);
+		ar & BOOST_SERIALIZATION_NVP(inp_max_);
+		ar & BOOST_SERIALIZATION_NVP(outp_min_);
+		ar & BOOST_SERIALIZATION_NVP(outp_max_);
+		computeLinearFactors();	// Only necessary when loading, but it doesn't hurt
+	}
+
+public:
+	sspLinearMap();
+	sspLinearMap(const sspLinearMap& val) = delete;
+	sspLinearMap& operator= (const sspLinearMap& val) = delete;
+	virtual ~sspLinearMap() {}
+
+	// Virtual methods
+	virtual float getValue() const override;
+	virtual bool verify(int& nErrors, int& nWarnings) const override;
+
+	// Accessors
+	void setValue(std::shared_ptr<sspValue> value) { val_ = std::move(value); }
+	
+	void setInputRange(float fMin, float fMax);		// This range may NOT be inverted
+	void setOutputRange(float fMin, float fMax);	// This range may be inverted
+
+	std::shared_ptr<sspValue> getInputValue() const { return val_; }
+
+	float getInputMin() const { return inp_min_; }
+	float getInputMax() const { return inp_max_; }
+	float getOutputMin() const { return outp_min_; }
+	float getOutputMax() const { return outp_max_; }
+};
+

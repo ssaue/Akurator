334c450d0cf8645743eefd9e710c14e950598c1c
Sigurd Saue <sigurd.saue@gmail.com>
2024-07-07 15:57:45 +0200

    Added a compile flag BUILD_HMM_TEST

--- src/projects/HMM.cpp
+++ src/projects/HMM.cpp
@@ -22,6 +22,8 @@
 #include "domain/core/sspPlayTask.h"
 #include "domain/elements/messages/sspConditionalMsgList.h"
 
+//#define BUILD_HMM_TEST 1
+
 namespace {
 	std::string file_root = "C:/HMM/MIDI/";
 }
@@ -78,6 +80,16 @@ void HMM::buildTimelineHierarchy(sspDomainData* domain)
 
 void HMM::buildPlayer(sspDomainData* domain)
 {
+
+#ifdef BUILD_HMM_TEST
+
+	auto midi_path_str = std::make_shared<sspSimpleString>();
+	midi_path_str->setString(file_root + "Test/");
+	midi_path_str->setName(midi_path_str->getString());
+	domain->getStrings().push_back(midi_path_str);
+
+#else
+
 	// Conditionals
 	auto time_of_day = std::make_shared<sspTimeCompare>();
 	time_of_day->setName("Before 16:00");
@@ -102,16 +114,18 @@ void HMM::buildPlayer(sspDomainData* domain)
 	sspWeakVector<sspConditional> conds;
 	conds.push_back(time_of_day);
 
-	auto cond_str = std::make_shared<sspConditionalString>();
-	cond_str->setName("Dag eller kveld");
-	cond_str->setConditionals(conds);
-	cond_str->setStrings(folder_strings);
-	cond_str->setDefaultString(evening_folder);
-	domain->getStrings().push_back(cond_str);
+	auto midi_path_str = std::make_shared<sspConditionalString>();
+	midi_path_str->setName("Dag eller kveld");
+	midi_path_str->setConditionals(conds);
+	midi_path_str->setStrings(folder_strings);
+	midi_path_str->setDefaultString(evening_folder);
+	domain->getStrings().push_back(midi_path_str);
+
+#endif
 
 	auto file = std::make_shared<sspFileString>();
 	file->setName("Midi path");
-	file->setPath(cond_str);
+	file->setPath(midi_path_str);
 	file->setAudioOnly(false);
 	file->setRecursiveSearch(false);
 	domain->getStrings().push_back(file);
@@ -131,6 +145,16 @@ void HMM::buildPlayer(sspDomainData* domain)
 	task->setPriority(sspPlayTask::Priority::Cancel);
 	domain->getPlaytasks().push_back(task);
 
+#ifdef BUILD_HMM_TEST
+
+	// Play on average every second hour
+	auto play_interval = std::make_shared<sspBasicValue>();
+	play_interval->setName("10 seconds");
+	play_interval->setValue(10);
+	domain->getValues().push_back(play_interval);
+
+#else
+
 	// Play on average every second hour
 	auto val_hour = std::make_shared<sspBasicValue>();
 	val_hour->setName("1 hour");
@@ -142,17 +166,19 @@ void HMM::buildPlayer(sspDomainData* domain)
 	val_3hour->setValue(10800);
 	domain->getValues().push_back(val_3hour);
 
-	auto random = std::make_shared<sspRandomValue>();
-	random->setName("1 to 3 hours");
-	random->setLow(val_hour);
-	random->setHigh(val_3hour);
-	domain->getValues().push_back(random);
+	auto play_interval = std::make_shared<sspRandomValue>();
+	play_interval->setName("1 to 3 hours");
+	play_interval->setLow(val_hour);
+	play_interval->setHigh(val_3hour);
+	domain->getValues().push_back(play_interval);
+
+#endif
 
 	// Add recurring load event 
 	auto msg_recv = std::make_unique<sspMessageWithReceiver>();
 	sspMessage& msg = msg_recv->getMessage();
 	msg.setTask(task);
-	msg.setTime(random);
+	msg.setTime(play_interval);
 	msg.setType(sspMessage::Type::Load);
 	msg_recv->setReceiver(domain->getTimelines()[1]);
 
@@ -162,17 +188,31 @@ void HMM::buildPlayer(sspDomainData* domain)
 	auto cond_msg = std::make_shared<sspConditionalMsgList>();
 	cond_msg->add(domain->getConditionals()[0], msglist);
 
+#ifdef BUILD_HMM_TEST
+
+	task->setMessageList(sspPlayTask::Messages::Exit, cond_msg);
+
+#else
+
 	task->setMessageList(sspPlayTask::Messages::Enter, cond_msg);
+
+#endif
 }
 
 void HMM::buildStartList(sspDomainData* domain)
 {
+	// Start after 10 seconds
+	auto val_10 = std::make_shared<sspBasicValue>();
+	val_10->setName("10 seconds");
+	val_10->setValue(10);
+	domain->getValues().push_back(val_10);
+
 	auto msglist = std::make_shared<sspMessageList>();
 
 	auto msg_recv = std::make_unique<sspMessageWithReceiver>();
 	sspMessage& msg = msg_recv->getMessage();
 	msg.setTask(domain->getPlaytasks()[0]);
-	msg.setTime(domain->getValues()[1]);
+	msg.setTime(val_10);
 	msg.setType(sspMessage::Type::Load);
 	msg_recv->setReceiver(domain->getTimelines()[1]);
 	msglist->add(std::move(msg_recv));
